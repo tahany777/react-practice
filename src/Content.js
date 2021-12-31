0eecf39c439fe0683df54c5ed87a16886a3018dd
tahany <tahanymohammed777@hotmail.com>
2021-12-31 10:13:36 +0200

    feat: destructuring and manage state

--- src/Content.js
+++ src/Content.js
@@ -1,56 +1,7 @@
 import React from "react";
-import { useState } from "react";
 import { FaTrashAlt } from "react-icons/fa";
-function Content() {
-  // const handleNameChange = () => {
-  //   const names = ["Bob", "Kevin", "Dave"];
-  //   const int = Math.floor(Math.random() * 3);
-  //   return names[int];
-  // };
-  // const handleClick = () => {
-  //   console.log('You Clicked it')
-  // }
-  // const handleClick2 = (name) => {
-  //   console.log(`${name} was clicked`)
-  // }
-  // const handleClick3 = (e) => {
-  //   console.log(e.target.innerText)
-  // }
-  // const [name, setName] = useState('Tahany');
-  // const handleNameChange = () => {
-  //   const names = ['Tahany', "Bob", "Kevin", "Dave"];
-  //   const int = Math.floor(Math.random() * 3);
-  //   setName(names[int]);
-  // };
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "Item 1",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3",
-    },
-  ]);
-  const handleCheck = (id) => {
-    const listItems = items.map((item) =>
-      item.id === id ? { ...item, checked: !item.checked } : item
-    );
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-  };
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-  };
+function Content({ items, handleCheck, handleDelete }) {
+
   return (
     <main>
       {items.length ? (
