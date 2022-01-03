93b468079d5210169cf4932ebca1afd2626f65b3
tahany <tahanymohammed777@hotmail.com>
2022-01-03 22:05:05 +0200

    feat: search bar added with functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import Header from "./Header";
+import SearchItem from "./SearchItem";
 import Content from "./Content";
 import Footer from "./Footer";
 import { useState } from "react";
 import AddItem from "./AddItem";
+
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
-  const [newItem, setNewItem] = useState('');
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("shoppinglist"))
+  );
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
 
   const setAndSaveItems = (newItems) => {
     setItems(newItems);
     localStorage.setItem("shoppinglist", JSON.stringify(newItems));
-  }
+  };
   const addItem = (item) => {
-    const id = items.length ? items[items.length - 1].id + 1 : 1;//get the last item
+    const id = items.length ? items[items.length - 1].id + 1 : 1; //get the last item
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
     setAndSaveItems(listItems);
-  }
+  };
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
@@ -29,21 +34,22 @@ function App() {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(!newItem) return;
+    if (!newItem) return;
     //addItem
     addItem(newItem);
-    setNewItem('');
+    setNewItem("");
   };
   return (
     <div className="App">
       <Header title="Grocery List" />
-      <AddItem 
-      newItem={newItem}
-      setNewItem={setNewItem}
-      handleSubmit={handleSubmit}
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
       />
+      <SearchItem search={search} setSearch={setSearch} />
       <Content
-        items={items}
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
