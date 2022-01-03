b2221736ec0f324bafa6415749327857bd5c803b
tahany <tahanymohammed777@hotmail.com>
2022-01-03 22:09:25 +0200

    feat: use Reference

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { FaPlus } from 'react-icons/fa'
+import { FaPlus } from "react-icons/fa";
+import { useRef } from "react";
 function AddItem({ newItem, setNewItem, handleSubmit }) {
+  const inputRef = useRef();
   return (
     <form className="addForm" onSubmit={handleSubmit}>
       <label htmlFor="addItem">Add Item</label>
       <input
         autoFocus
+        ref={inputRef}
         id="addItem"
         type="text"
         placeholder="Add Item"
@@ -13,9 +16,12 @@ function AddItem({ newItem, setNewItem, handleSubmit }) {
         value={newItem}
         onChange={(e) => setNewItem(e.target.value)}
       />
-      <button type="submit"
-      aria-label='Add Item'>
-          <FaPlus />
+      <button
+        type="submit"
+        aria-label="Add Item"
+        onClick={() => inputRef.current.focus()}
+      >
+        <FaPlus />
       </button>
     </form>
   );
