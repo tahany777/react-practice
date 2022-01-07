1f72837ed2d3925393e53c451981b660e870f54b
tahany <tahanymohammed777@hotmail.com>
2022-01-07 18:10:00 +0200

    feat: use effect with local storage

--- src/App.js
+++ src/App.js
@@ -2,36 +2,37 @@ import Header from "./Header";
 import SearchItem from "./SearchItem";
 import Content from "./Content";
 import Footer from "./Footer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddItem from "./AddItem";
 
 function App() {
-  const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("shoppinglist"))
-  );
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem("shoppinglist")) || []);
   const [newItem, setNewItem] = useState("");
   const [search, setSearch] = useState("");
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(newItems));
-  };
+  useEffect(() => {
+    localStorage.setItem("shoppinglist", JSON.stringify(items));//save items in local storage
+  }, [items]);
+
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1; //get the last item
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
+
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
+
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!newItem) return;
@@ -39,6 +40,7 @@ function App() {
     addItem(newItem);
     setNewItem("");
   };
+
   return (
     <div className="App">
       <Header title="Grocery List" />
@@ -49,7 +51,9 @@ function App() {
       />
       <SearchItem search={search} setSearch={setSearch} />
       <Content
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+        items={items.filter((item) =>
+          item.item.toLowerCase().includes(search.toLowerCase())
+        )}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
